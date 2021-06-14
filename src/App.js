72f6836112d5cb00758e71be96f3748a3f502ed3
Vanessadu <a787293136@gmail.com>
2021-06-14 14:00:37 -0400

    implemented firebase utils, include ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import HomePage from './components/pages/homepage/homepage.component';
 import ShopPage from './components/pages/shop/shop.component';
 import SignInAndSignUpPage from './components/pages/sign-in and sign-up/sign-in and sign-up.component';
 import { Switch, Route } from 'react-router-dom';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
+
 
 
 
@@ -21,8 +23,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+          console.log(this.state);
+        })
+      }
+      else {
+        this.setState({ currentUser: userAuth });
+      }
     });
     }
 
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -15,9 +15,9 @@ const Header = ({ currentUser}) => (
             <Link className='option' to='/shop'>CONTACT</Link>
             {
                 currentUser ?
-                <div className='option' onclick={()=> auth.signOut()}>SIGN OUT</div>
+                (<div className='option' onClick={()=> auth.signOut()}>SIGN OUT</div>)
                 :
-                <Link className='option' to='/signin'>SIGN IN</Link>
+                (<Link className='option' to='/signin'>SIGN IN</Link>)
             }
         </div>
     </div>
