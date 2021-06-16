24df21e8b5f2af5511558fbbb77681052b0f8919
Vanessadu <a787293136@gmail.com>
2021-06-16 12:14:39 -0400

    added checkout page, checkout item component and updated cart to use selectors from reselect

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropDown from '../cart-dropdown/cart-dropdown.component'; 
+import { selectCartHidden } from '../../redux/cart/cart.selector';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser, hidden}) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -20,23 +23,23 @@ const Header = ({ currentUser, hidden}) => (
             <Link className='option' to='/shop'>CONTACT</Link>
             {
                 currentUser ?
-                (<div className='option' onClick={()=> auth.signOut()}>SIGN OUT</div>)
-                :
-                (<Link className='option' to='/signin'>SIGN IN</Link>)
+                    (<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>)
+                    :
+                    (<Link className='option' to='/signin'>SIGN IN</Link>)
             }
-        <CartIcon />
+            <CartIcon />
         </div>
         {
-           hidden? null : 
-            <CartDropDown />
+            hidden ? null :
+                <CartDropDown />
 
         }
     </div>
-)
+);
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden}}) => ({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header); 
